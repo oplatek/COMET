9e6208cd9e462826cbde73fb41d9daa28fb95975
Ondrej Platek <ondrej.platek@seznam.cz>
2024-09-08 23:58:14 +0200

    add run.sh to change the exp dir

---
+++ run.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#
+# expected to be run by gpu-bash run.sh on slurm
+CMD="python"  # if forexample "echo python" ... then it is dry run
+MAIN_CFG="da/configs/cometkiwi_finetuning.yaml"
+TRAINER_CFG="da/configs/trainer.yaml"
+EARLY_STOP_CFG="da/configs/early_stopping.yaml"
+CKPT_CFG="da/configs/model_checkpoint.yaml"
+CKPT="$HOME/.cache/huggingface/hub/models--Unbabel--wmt22-cometkiwi-da/snapshots/b3a8aea5a5fc22db68a554b92b3d96eb6ea75cc9/checkpoints/model.ckpt"
+
+. parse_options.sh
+
+set -x
+GIT_COMMIT="$(git rev-parse --short HEAD)"
+TIMESTAMP="$(date -u +'%Y%m%dT%H%M%S')"
+
+if [[ -z $SLURM_JOB_ID ]] ; then
+  printf "Not a SLURM job" ; exit 1
+fi
+EXPERIMENT_ID="Z_$TIMESTAMP.$SLURM_JOB_ID.$GIT_COMMIT"
+
+
+mkdir -p "exp/$EXPERIMENT_ID/"
+
+cat $MAIN_CFG > "exp/$EXPERIMENT_ID/$(basename $MAIN_CFG)"
+cat $TRAINER_CFG > "exp/$EXPERIMENT_ID/$(basename $TRAINER_CFG)"
+cat $EARLY_STOP_CFG > "exp/$EXPERIMENT_ID/$(basename $EARLY_STOP_CFG)"
+sed \
+  -e "s;dirpath: TODO-dirpath;dirpath: exp/$EXPERIMENT_ID;" \
+  $CKPT_CFG \
+    > exp/$EXPERIMENT_ID/$(basename $CKPT_CFG)
+
+# hack https://github.com/Lightning-AI/pytorch-lightning/issues/5225#issuecomment-750032030
+export UNUSED_SLURM_NTAKS=$SLURM_NTASK
+export UNUSED_SLURM_JOB_NAME=$SLURM_JOB_NAME
+unset SLURM_NTASK SLURM_JOB_NAME
+
+$CMD \
+  ./comet/cli/train.py \
+  --load_from_checkpoint "$CKPT" \
+  --cfg "exp/$EXPERIMENT_ID/$(basename $MAIN_CFG)"
